8b84d8ba13e89e0c9abe1a9a427e9c1aa34235a1
Gianni Bussoletti <giannibussoletti@gmail.com>
2026-07-10 10:46:43 +0200

    Create Entity Dipendente

---
+++ src/main/java/gianni_bussoletti/beu2s2exam/entities/Dipendente.java
@@ -0,0 +1,37 @@
+package gianni_bussoletti.beu2s2exam.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@ToString
+@Table(name = "dipendenti")
+public class Dipendente {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+    @Column(nullable = false)
+    private String username;
+    @Column(nullable = false)
+    private String nome;
+    @Column(nullable = false)
+    private String cognome;
+    @Column(nullable = false)
+    private String email;
+
+    public Dipendente(String username, String nome, String cognome, String email) {
+        this.username = username;
+        this.nome = nome;
+        this.cognome = cognome;
+        this.email = email;
+    }
+}
